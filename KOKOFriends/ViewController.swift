e50a78192a940caeaaade0592d083bd46a44f523
Yen Lin <e19970722@gmail.com>
2025-05-21 20:46:15 +0800

    Initial Commit

---
+++ KOKOFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOKOFriends
+//
+//  Created by Yen Lin on 2025/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
